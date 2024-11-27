e751bb5157d26a972d62b661c72e89de65fc8d93
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-11-27 13:15:29 +0900

    Update parking_details.js

--- demo/src/main/resources/static/js/parking_details.js
+++ demo/src/main/resources/static/js/parking_details.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("페이지가 로드되었습니다.");
 
-    // CSRF 토큰을 가져오는 함수
     function getCsrfToken() {
         return {
             token: document.querySelector('meta[name="_csrf"]').getAttribute('content'),
             header: document.querySelector('meta[name="_csrf_header"]').getAttribute('content')
         };
     }
-
-    // CSRF 토큰을 가져와서 콘솔에 출력 (디버깅 목적)
     const csrf = getCsrfToken();
     console.log("CSRF Token:", csrf.token);
     console.log("CSRF Header:", csrf.header);
 
-    // AJAX 요청에 CSRF 토큰을 포함시키는 설정
     document.querySelector('form').addEventListener('submit', function(event) {
         event.preventDefault();
 
