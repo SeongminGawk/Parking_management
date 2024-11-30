f99ebf49b8c4511275a5295141d066fe8d2e7dbe
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-11-30 18:25:55 +0900

    주차장 데이터 관리

--- src/main/java/com/example/demo/repository/ParkingRepository.java
+++ src/main/java/com/example/demo/repository/ParkingRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.entity.ParkingLot;
 
-
 @Repository
 public interface ParkingRepository extends JpaRepository<ParkingLot, Integer> {
     ParkingLot findTopByRgtrIdOrderByRegDtDesc(String rgtrId);
