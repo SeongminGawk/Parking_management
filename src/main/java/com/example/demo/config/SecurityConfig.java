7d2e387eab0f71a716a4493918454d3c7bed387a
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-11-30 18:23:19 +0900

    사용자 인증 및 권한 관리

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
-
     @Value("${file.uploadDir}")
     private String uploadDir;
 
