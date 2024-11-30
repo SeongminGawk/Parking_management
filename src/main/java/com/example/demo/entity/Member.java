83a9ca7f96cc0dd4854060ec11221d2c801d243c
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-11-30 17:52:44 +0900

    회원 관리를 위한 Member 엔티티 추가

--- src/main/java/com/example/demo/entity/Member.java
+++ src/main/java/com/example/demo/entity/Member.java
@@ -1,14 +1,12 @@
 package com.example.demo.entity;
 
 import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,7 +15,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Member {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mbr_seq")
