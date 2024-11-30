49564accc782ede892d1364b75870f5b4bde27d8
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-11-30 17:34:47 +0900

    LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -24,7 +24,7 @@ public class LoginController {
     private final MemberLoginService memberLoginService;
     private final RSAKeyService rsaKeyService;
 
-    @Autowired // 의존성 주입을 통해 MemberLoginService와 RSAKeyService 인스턴스를 생성자에서 받아 초기화합니다.
+    @Autowired 
     public LoginController(MemberLoginService memberLoginService, RSAKeyService rsaKeyService) {
         this.memberLoginService = memberLoginService;
         this.rsaKeyService = rsaKeyService;
@@ -32,20 +32,20 @@ public class LoginController {
 
     @GetMapping("/login")
     public String showLoginPage(Model model) {
-        String publicKey = rsaKeyService.getPublicKey(); // RSA 공개 키 가져오기
-        model.addAttribute("publicKey", publicKey); // 모델에 키 추가
+        String publicKey = rsaKeyService.getPublicKey(); 
+        model.addAttribute("publicKey", publicKey);
         return "login";
     }
 
     @PostMapping("/perform_login")
-    @ResponseBody // POST 요청에 대해 /perform_login 경로를 매핑합니다. 로그인 요청을 처리하고 JSON 형식의 응답을 반환합니다.
+    @ResponseBody 
     public Map<String, Object> performLogin(@RequestParam("encryptedUserid") String encryptedUserid, @RequestParam("encryptedPassword") String encryptedPassword, HttpSession session) {
         Map<String, Object> response = new HashMap<>();
         try {
             if (memberLoginService.validateLogin(encryptedUserid, encryptedPassword)) {
                 String userid = memberLoginService.decryptWithRSA(encryptedUserid);
                 session.setAttribute("userid", userid);
-                response.put("success", true); //성공하면 아이디 저장
+                response.put("success", true); 
             } else {
                 response.put("success", false);
                 response.put("message", "아이디 및 비밀번호를 다시 입력해 주십시오.");
@@ -60,9 +60,9 @@ public class LoginController {
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession(false);
         if (session != null) {
-            session.invalidate(); // 세션 무효화하여 로그아웃 처리
+            session.invalidate(); 
         }
-        return "redirect:/login"; // 로그아웃 후 로그인 페이지로 이동
+        return "redirect:/login";
     }
 }
 
