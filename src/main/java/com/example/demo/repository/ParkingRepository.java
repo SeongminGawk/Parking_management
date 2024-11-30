2d25dc4831d6ce9ea6a025177ca977b4a5f83467
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-11-30 18:25:44 +0900

    Update ParkingRepository.java

--- src/main/java/com/example/demo/repository/ParkingRepository.java
+++ src/main/java/com/example/demo/repository/ParkingRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.entity.ParkingLot;
 
+
 @Repository
 public interface ParkingRepository extends JpaRepository<ParkingLot, Integer> {
     ParkingLot findTopByRgtrIdOrderByRegDtDesc(String rgtrId);
